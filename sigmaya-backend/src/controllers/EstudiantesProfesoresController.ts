c014b1ee408b461039064accebec05cfc22b54b6
Nekstoreo <nestorg456k@outlook.com>
2024-11-03 21:04:21 -0500

    feat: refactor EstudiantesProfesoresController to use EstudiantesProfesoresService for improved code organization

--- sigmaya-backend/src/controllers/EstudiantesProfesoresController.ts
+++ sigmaya-backend/src/controllers/EstudiantesProfesoresController.ts
@@ -1,40 +1,28 @@
 // src/controllers/EstudiantesProfesoresController.ts
 import { Request, Response } from 'express';
-import {
-  EstudianteRepository,
-  ProfesorRepository,
-  CalificacionParcialRepository,
-  CalificacionFinalRepository,
-  InscripcionCursoRepository,
-  ProgramaEstudianteRepository,
-} from '../repositories/allRepositories';
+import { EstudiantesProfesoresService } from '../services/EstudiantesProfesoresService';
 
 export class EstudiantesProfesoresController {
-  private estudianteRepo = new EstudianteRepository();
-  private profesorRepo = new ProfesorRepository();
-  private calificacionParcialRepo = new CalificacionParcialRepository();
-  private calificacionFinalRepo = new CalificacionFinalRepository();
-  private inscripcionCursoRepo = new InscripcionCursoRepository();
-  private programaEstudianteRepo = new ProgramaEstudianteRepository();
+  private estudiantesProfesoresService = new EstudiantesProfesoresService();
 
   // tbl_estudiantes
   async getEstudiantes(req: Request, res: Response) {
-    const estudiantes = await this.estudianteRepo.findAll();
+    const estudiantes = await this.estudiantesProfesoresService.getEstudiantes();
     res.json(estudiantes);
   }
 
   async getEstudianteById(req: Request, res: Response) {
-    const estudiante = await this.estudianteRepo.findById(Number(req.params.id));
+    const estudiante = await this.estudiantesProfesoresService.getEstudianteById(Number(req.params.id));
     res.json(estudiante);
   }
 
   async createEstudiante(req: Request, res: Response) {
-    const nuevoEstudiante = await this.estudianteRepo.createEstudiante(req.body);
+    const nuevoEstudiante = await this.estudiantesProfesoresService.createEstudiante(req.body);
     res.json(nuevoEstudiante);
   }
 
   async updateEstudiante(req: Request, res: Response) {
-    const estudiante = await this.estudianteRepo.updateEstudiante(
+    const estudiante = await this.estudiantesProfesoresService.updateEstudiante(
       Number(req.params.id),
       req.body
     );
@@ -42,28 +30,28 @@ export class EstudiantesProfesoresController {
   }
 
   async deleteEstudiante(req: Request, res: Response) {
-    const success = await this.estudianteRepo.deleteEstudiante(Number(req.params.id));
+    const success = await this.estudiantesProfesoresService.deleteEstudiante(Number(req.params.id));
     res.json({ success });
   }
 
   // tbl_profesores
   async getProfesores(req: Request, res: Response) {
-    const profesores = await this.profesorRepo.findAll();
+    const profesores = await this.estudiantesProfesoresService.getProfesores();
     res.json(profesores);
   }
 
   async getProfesorById(req: Request, res: Response) {
-    const profesor = await this.profesorRepo.findById(Number(req.params.id));
+    const profesor = await this.estudiantesProfesoresService.getProfesorById(Number(req.params.id));
     res.json(profesor);
   }
 
   async createProfesor(req: Request, res: Response) {
-    const nuevoProfesor = await this.profesorRepo.createProfesor(req.body);
+    const nuevoProfesor = await this.estudiantesProfesoresService.createProfesor(req.body);
     res.json(nuevoProfesor);
   }
 
   async updateProfesor(req: Request, res: Response) {
-    const profesor = await this.profesorRepo.updateProfesor(
+    const profesor = await this.estudiantesProfesoresService.updateProfesor(
       Number(req.params.id),
       req.body
     );
@@ -71,30 +59,30 @@ export class EstudiantesProfesoresController {
   }
 
   async deleteProfesor(req: Request, res: Response) {
-    const success = await this.profesorRepo.deleteProfesor(Number(req.params.id));
+    const success = await this.estudiantesProfesoresService.deleteProfesor(Number(req.params.id));
     res.json({ success });
   }
 
   // tbl_calificaciones_parciales
   async getCalificacionesParciales(req: Request, res: Response) {
-    const calificaciones = await this.calificacionParcialRepo.findAll();
+    const calificaciones = await this.estudiantesProfesoresService.getCalificacionesParciales();
     res.json(calificaciones);
   }
 
   async getCalificacionParcialById(req: Request, res: Response) {
-    const calificacion = await this.calificacionParcialRepo.findById(Number(req.params.id));
+    const calificacion = await this.estudiantesProfesoresService.getCalificacionParcialById(Number(req.params.id));
     res.json(calificacion);
   }
 
   async createCalificacionParcial(req: Request, res: Response) {
-    const nuevaCalificacion = await this.calificacionParcialRepo.createCalificacionParcial(
+    const nuevaCalificacion = await this.estudiantesProfesoresService.createCalificacionParcial(
       req.body
     );
     res.json(nuevaCalificacion);
   }
 
   async updateCalificacionParcial(req: Request, res: Response) {
-    const calificacion = await this.calificacionParcialRepo.updateCalificacionParcial(
+    const calificacion = await this.estudiantesProfesoresService.updateCalificacionParcial(
       Number(req.params.id),
       req.body
     );
@@ -102,7 +90,7 @@ export class EstudiantesProfesoresController {
   }
 
   async deleteCalificacionParcial(req: Request, res: Response) {
-    const success = await this.calificacionParcialRepo.deleteCalificacionParcial(
+    const success = await this.estudiantesProfesoresService.deleteCalificacionParcial(
       Number(req.params.id)
     );
     res.json({ success });
@@ -110,24 +98,24 @@ export class EstudiantesProfesoresController {
 
   // tbl_calificaciones_finales
   async getCalificacionesFinales(req: Request, res: Response) {
-    const calificaciones = await this.calificacionFinalRepo.findAll();
+    const calificaciones = await this.estudiantesProfesoresService.getCalificacionesFinales();
     res.json(calificaciones);
   }
 
   async getCalificacionFinalById(req: Request, res: Response) {
-    const calificacion = await this.calificacionFinalRepo.findById(Number(req.params.id));
+    const calificacion = await this.estudiantesProfesoresService.getCalificacionFinalById(Number(req.params.id));
     res.json(calificacion);
   }
 
   async createCalificacionFinal(req: Request, res: Response) {
-    const nuevaCalificacion = await this.calificacionFinalRepo.createCalificacionFinal(
+    const nuevaCalificacion = await this.estudiantesProfesoresService.createCalificacionFinal(
       req.body
     );
     res.json(nuevaCalificacion);
   }
 
   async updateCalificacionFinal(req: Request, res: Response) {
-    const calificacion = await this.calificacionFinalRepo.updateCalificacionFinal(
+    const calificacion = await this.estudiantesProfesoresService.updateCalificacionFinal(
       Number(req.params.id),
       req.body
     );
@@ -135,7 +123,7 @@ export class EstudiantesProfesoresController {
   }
 
   async deleteCalificacionFinal(req: Request, res: Response) {
-    const success = await this.calificacionFinalRepo.deleteCalificacionFinal(
+    const success = await this.estudiantesProfesoresService.deleteCalificacionFinal(
       Number(req.params.id)
     );
     res.json({ success });
@@ -143,24 +131,24 @@ export class EstudiantesProfesoresController {
 
   // tbl_inscripciones_curso
   async getInscripcionesCurso(req: Request, res: Response) {
-    const inscripciones = await this.inscripcionCursoRepo.findAll();
+    const inscripciones = await this.estudiantesProfesoresService.getInscripcionesCursos();
     res.json(inscripciones);
   }
 
   async getInscripcionCursoById(req: Request, res: Response) {
-    const inscripcion = await this.inscripcionCursoRepo.findById(Number(req.params.id));
+    const inscripcion = await this.estudiantesProfesoresService.getInscripcionCursoById(Number(req.params.id));
     res.json(inscripcion);
   }
 
   async createInscripcionCurso(req: Request, res: Response) {
-    const nuevaInscripcion = await this.inscripcionCursoRepo.createInscripcionCurso(
+    const nuevaInscripcion = await this.estudiantesProfesoresService.createInscripcionCurso(
       req.body
     );
     res.json(nuevaInscripcion);
   }
 
   async updateInscripcionCurso(req: Request, res: Response) {
-    const inscripcion = await this.inscripcionCursoRepo.updateInscripcionCurso(
+    const inscripcion = await this.estudiantesProfesoresService.updateInscripcionCurso(
       Number(req.params.id),
       req.body
     );
@@ -168,7 +156,7 @@ export class EstudiantesProfesoresController {
   }
 
   async deleteInscripcionCurso(req: Request, res: Response) {
-    const success = await this.inscripcionCursoRepo.deleteInscripcionCurso(
+    const success = await this.estudiantesProfesoresService.deleteInscripcionCurso(
       Number(req.params.id)
     );
     res.json({ success });
@@ -176,24 +164,24 @@ export class EstudiantesProfesoresController {
 
   // tbl_programas_estudiantes
   async getProgramasEstudiantes(req: Request, res: Response) {
-    const programas = await this.programaEstudianteRepo.findAll();
+    const programas = await this.estudiantesProfesoresService.getProgramasEstudiantes();
     res.json(programas);
   }
 
   async getProgramaEstudianteById(req: Request, res: Response) {
-    const programa = await this.programaEstudianteRepo.findById(Number(req.params.id));
+    const programa = await this.estudiantesProfesoresService.getProgramaEstudianteById(Number(req.params.id));
     res.json(programa);
   }
 
   async createProgramaEstudiante(req: Request, res: Response) {
-    const nuevoPrograma = await this.programaEstudianteRepo.createProgramaEstudiante(
+    const nuevoPrograma = await this.estudiantesProfesoresService.createProgramaEstudiante(
       req.body
     );
     res.json(nuevoPrograma);
   }
 
   async updateProgramaEstudiante(req: Request, res: Response) {
-    const programa = await this.programaEstudianteRepo.updateProgramaEstudiante(
+    const programa = await this.estudiantesProfesoresService.updateProgramaEstudiante(
       Number(req.params.id),
       req.body
     );
@@ -201,7 +189,7 @@ export class EstudiantesProfesoresController {
   }
 
   async deleteProgramaEstudiante(req: Request, res: Response) {
-    const success = await this.programaEstudianteRepo.deleteProgramaEstudiante(
+    const success = await this.estudiantesProfesoresService.deleteProgramaEstudiante(
       Number(req.params.id)
     );
     res.json({ success });
