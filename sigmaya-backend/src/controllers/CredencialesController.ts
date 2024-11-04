54cd502f9700b5a84ee8be56ee09ac007780f5ea
Nekstoreo <nestorg456k@outlook.com>
2024-11-03 20:56:14 -0500

    feat: refactor CredencialesController to use CredencialesService for better separation of concerns

--- sigmaya-backend/src/controllers/CredencialesController.ts
+++ sigmaya-backend/src/controllers/CredencialesController.ts
@@ -1,31 +1,31 @@
 import { Request, Response } from "express";
-import { CredencialRepository } from "../repositories/allRepositories";
+import { CredencialesService } from "../services/CredencialesService";
 
 export class CredencialesController {
-  private credencialRepo = new CredencialRepository();
+  private credencialesService = new CredencialesService();
 
   async getCredenciales(req: Request, res: Response) {
-    const credenciales = await this.credencialRepo.findAll();
+    const credenciales = await this.credencialesService.getCredenciales();
     res.json(credenciales);
   }
 
   async getCredencialById(req: Request, res: Response) {
-    const credencial = await this.credencialRepo.findById(Number(req.params.id));
+    const credencial = await this.credencialesService.getCredencialById(Number(req.params.id));
     res.json(credencial);
   }
 
   async createCredencial(req: Request, res: Response) {
-    const nuevaCredencial = await this.credencialRepo.createCredencial(req.body);
+    const nuevaCredencial = await this.credencialesService.createCredencial(req.body);
     res.json(nuevaCredencial);
   }
 
   async updateCredencial(req: Request, res: Response) {
-    const credencial = await this.credencialRepo.updateCredencial(Number(req.params.id), req.body);
+    const credencial = await this.credencialesService.updateCredencial(Number(req.params.id), req.body);
     res.json(credencial);
   }
 
   async deleteCredencial(req: Request, res: Response) {
-    const success = await this.credencialRepo.deleteCredencial(Number(req.params.id));
+    const success = await this.credencialesService.deleteCredencial(Number(req.params.id));
     res.json({ success });
   }
 }
